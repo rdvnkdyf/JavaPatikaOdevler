19daf17f316e11c113de9c81be982c2763e12450
rdvnkdyf <49274168+rdvnkdyf@users.noreply.github.com>
2021-07-03 00:22:48 +0300

    Add files via upload

---
+++ HarmonikSeriBulma.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class HarmonikSeriBulma {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("N sayısını giriniz :");
+        double n = input.nextInt();
+        double result = 0;
+        while (n > 0) {
+            result += (1 / n);
+            n--;
+        }
+
+        System.out.println(result);
+    }
+}
