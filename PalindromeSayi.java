22eb7f69e5359161bd0b2bf070a3938dee9248bd
rdvnkdyf <49274168+rdvnkdyf@users.noreply.github.com>
2021-07-07 20:58:43 +0300

    Add files via upload

---
+++ PalindromeSayi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class PalindromeSayi{
+    static boolean isPalindrom(int number){
+        int temp=number,reverseNumber=0,mod;
+
+        while(temp!=0){
+            mod=temp%10;
+            reverseNumber=(reverseNumber*10)+mod;
+            temp/=10;
+        }
+        return number==reverseNumber;
+    }
+    public static void main(String[]args){
+       System.out.println(isPalindrom(4004));
+    }
+}
